9bb61bcc373b2b0dcad3369739a48fd1a0c47b30
wagner.esser <wagner.esser@ngi.com.br>
2021-03-24 22:01:27 -0300

    style(category): stylization of the list of categories

---
+++ src/pages/registrations/styles.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+
+export const CategoryWrapper = styled.div`
+    padding: 50px;
+    display: flex;
+`
+
+export const PageDivisor = styled.div`
+    flex: 1;
+`
+
+export const CategoryList = styled.ul`
+    list-style: none;
+    padding-left: 0%;
+
+    & > li {
+        margin-bottom: 10px;
+        display: flex;
+    }
+`
+
+interface ICircleProps {
+    color: string
+}
+
+export const Circle = styled.div<ICircleProps>`
+    background-color: ${({ color }) => color};
+    width: 15px;
+    height: 15px;
+    border-radius: 100%;
+    margin-right: 10px;
+`
+
+export const Form = styled.form`
+    margin-bottom: 20px;
+`
